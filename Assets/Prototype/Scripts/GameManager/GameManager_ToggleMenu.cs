d2d195abb01f0dd41e258a58d79583b9feb2c63d
Manish jha <devmanjha@gmail.com>
2020-08-16 14:47:47 +0530

    Initial commit

---
+++ Assets/Prototype/Scripts/GameManager/GameManager_ToggleMenu.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace GM
+{
+    public class GameManager_ToggleMenu : MonoBehaviour
+    {
+        private GameManager_Master gameManagerMaster;
+        public GameObject menu;
+        public string pauseButtonName;
+
+        void Start()
+        {
+            ToggleMenu();
+        }
+
+        void Update()
+        {
+            CheckForMenuToggleRequest();
+        }
+
+        private void OnEnable()
+        {
+            SetInitialReferences();
+            gameManagerMaster.GameOverEvent += ToggleMenu;
+        }
+
+        private void OnDisable()
+        {
+            gameManagerMaster.GameOverEvent -= ToggleMenu;
+        }
+
+        void SetInitialReferences()
+        {
+            gameManagerMaster = GetComponent<GameManager_Master>();
+        }
+
+        void CheckForMenuToggleRequest()
+        {
+            if (bl_MobileInput.GetButtonUp(pauseButtonName) && !gameManagerMaster.isGameOver && !gameManagerMaster.isInventoryUIOn)
+            {
+                ToggleMenu();
+            }
+        }
+
+        void ToggleMenu()
+        {
+            if (menu != null)
+            {
+                menu.SetActive(!menu.activeSelf);
+                gameManagerMaster.isMenuOn = !gameManagerMaster.isMenuOn;
+                gameManagerMaster.CallEventMenuToggle();
+            }
+            else
+            {
+                Debug.LogWarning("You need to assign a UI GameObject to ToggleMenu in inspector");
+            }
+        }
+    }
+}
---
+++ Assets/Prototype/Scripts/GameManager/GameManager_TogglePlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace GM
+{
+    public class GameManager_TogglePlayer : MonoBehaviour
+    {
+        public FirstPersonController playerController;
+        private GameManager_Master gameManagerMaster;
+
+        private void OnEnable()
+        {
+            SetInitialReferences();
+            gameManagerMaster.MenuToggleEvent += TogglePlayerController;
+            gameManagerMaster.InventoryToggleEvent += TogglePlayerController;
+
+        }
+        private void OnDisable()
+        {
+            gameManagerMaster.MenuToggleEvent -= TogglePlayerController;
+            gameManagerMaster.InventoryToggleEvent -= TogglePlayerController;
+        }
+
+        void SetInitialReferences()
+        {
+            gameManagerMaster = GetComponent<GameManager_Master>();
+        }
+
+        void TogglePlayerController()
+        {
+            if (playerController != null)
+            {
+                playerController.enabled = !playerController.enabled;
+            }
+        }
+    }
+}
